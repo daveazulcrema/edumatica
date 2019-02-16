9643bd920fe5d47a0b5846063f2bbb9d00759ec6
daveazulcrema <47697921+daveazulcrema@users.noreply.github.com>
2019-02-16 12:57:42 -0600

    initial upload project 201902161256 pm

---
+++ Controllers/FacturasController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppDepartmentEmployee.Controllers
+{
+    public class FacturasController : Controller
+    {
+        // GET: Facturas
+        public ActionResult Index()
+        {
+            ViewBag.variable = "un valor";
+            return View();
+        }
+    }
+}
---
+++ Scripts/Modal.js
@@ -0,0 +1,17 @@
+﻿function popup() {
+    $.ajax({
+        type: 'get',
+        url: '/employees/Create',
+        data: {
+            'field1': 'hello',
+            'field2': 'hello1'
+        },
+        success: function (response) {
+            $("#show-modal").modal({ show: true, backdrop: 'static', keyboard: false })
+            $('#inner-show-modal').html(response);
+        },
+        error: function () {
+            alert("error");
+        }
+    });
+};
